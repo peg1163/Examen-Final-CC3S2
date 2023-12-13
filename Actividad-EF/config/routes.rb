04ef43bdfbf079d8f3224a7e32d2849bc192d3e6
Jaime Gonzalo Acuña Napan <jaime.acuna.n@uni.pe>
2023-12-13 09:51:56 -0500

    update

--- Actividad-EF/config/routes.rb
+++ Actividad-EF/config/routes.rb
@@ -2,5 +2,7 @@ Rottenpotatoes::Application.routes.draw do
     resources :movies
     # map '/' to be a redirect to '/movies'
     root :to => redirect('/movies')
+
+    get 'movies/search_tmdb', to: 'movies#search_tmdb', as: 'search_tmdb'
+
   end
-  
